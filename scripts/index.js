2ebf6425d6a624311e83bd5710ebc67f6ee92653
vandriacco <vandriacco@gmail.com>
2021-05-05 20:01:23 -0400

    committed

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+function updateTextInput(value) {
+    document.getElementById("tip-display").textContent = value
+}
+function calculateTip() {
+    // document.getElementById("input-cost").textcontent = cost
+    // let percent = updateTextInput(value)/100
+    // let tip = percent * cost
+    // return tip
+    let total = Number(document.getElementById("input-cost").value)
+    let percent = Number(document.getElementById("tip").value) / 100
+    let tip = total * percent
+    document.getElementById("tip-value").textContent = tip;
+    document.getElementById("cost-display").textContent = total + tip
+}
